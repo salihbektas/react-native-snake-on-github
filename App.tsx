0e9341bb73a40d55dbdef5284fc605c10cfa35cc
Salih Bektaş <sbektas@gtu.edu.tr>
2023-03-30 13:05:58 +0300

    feat: movement added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useRef } from 'react';
 import {
+  Animated,
   Button,
   SafeAreaView,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
 
@@ -12,43 +12,77 @@ function App(): JSX.Element {
 
   const tiles = []
 
+  const moveH = useRef(new Animated.Value(0)).current
 
-  for(let i = 0; i < 49; ++i)
+  const posH = useRef(0)
+
+  const moveV = useRef(new Animated.Value(0)).current
+
+  const posV = useRef(0)
+
+  for (let i = 0; i < 49; ++i)
     tiles.push(
-      <View style={{...styles.tile, backgroundColor: i%2 === 0 ? 'darkgrey' : 'lightgrey'}} key={i}/>
+      <View style={{ ...styles.tile, backgroundColor: i % 2 === 0 ? 'darkgrey' : 'lightgrey' }} key={i} />
     )
 
 
-  function up(){
-    console.log('up')
+  function up() {
+    posV.current -= 1
+    Animated.timing(moveV, {
+      toValue: posV.current,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
   }
-  function down(){
-    console.log('down')
+  function down() {
+    posV.current += 1
+    Animated.timing(moveV, {
+      toValue: posV.current,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
   }
-  function left(){
-    console.log('left')
+  function left() {
+    posH.current -= 1
+    Animated.timing(moveH, {
+      toValue: posH.current,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
   }
-  function right(){
-    console.log('right')
+  function right() {
+    posH.current += 1
+    Animated.timing(moveH, {
+      toValue: posH.current,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
   }
 
   return (
     <SafeAreaView style={styles.main}>
-      <Text>Deneme</Text>
+      <Animated.View style={{
+        height: 50, aspectRatio: 1, backgroundColor: 'red', zIndex: 1,
+        transform: [
+          { translateX: moveH.interpolate({ inputRange: [0, 1], outputRange: [0, 50] }) },
+          { translateY: moveV.interpolate({ inputRange: [0, 1], outputRange: [0, 50] }) }
+        ]
+      }}
+      />
       <View style={styles.board}>
         {tiles}
       </View>
       <View>
-      <Button title='up' onPress={up}/>
-      <View style={{flexDirection: 'row'}}>
-        <View style={{width:'50%'}}>
-          <Button title='left' onPress={left}/>
+        <Button title='up' onPress={up} />
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ width: '50%' }}>
+            <Button title='left' onPress={left} />
+          </View>
+          <View style={{ width: '50%' }}>
+            <Button title='right' onPress={right} />
+          </View>
         </View>
-        <View style={{width:'50%'}}>
-          <Button title='right' onPress={right}/>
-        </View>
-      </View>
-      <Button title='down' onPress={down}/>
+        <Button title='down' onPress={down} />
       </View>
     </SafeAreaView>
   );
@@ -68,7 +102,7 @@ const styles = StyleSheet.create({
 
   board: {
     flexDirection: 'row',
-    flexWrap:'wrap',
+    flexWrap: 'wrap',
     width: 350,
     aspectRatio: 1
   }
