a462635b6f09dd631746ddde7d2a4bd9e197899f
Salih Bektaş <sbektas@gtu.edu.tr>
2023-03-31 19:11:23 +0300

    feat: continuous movement added

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Animated,
   Button,
@@ -6,11 +6,14 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
+import useInterval from 'use-interval';
 
 
 function App(): JSX.Element {
 
   const tiles = []
+
+  const [direction, setDirection] = useState('')
 
   const moveH = useRef(new Animated.Value(0)).current
 
@@ -27,62 +30,85 @@ function App(): JSX.Element {
 
 
   function up() {
-    posV.current -= 1
-    Animated.timing(moveV, {
-      toValue: posV.current,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
+    if(posV.current > 0){
+      posV.current -= 1
+      Animated.timing(moveV, {
+        toValue: posV.current,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }
   }
   function down() {
-    posV.current += 1
-    Animated.timing(moveV, {
-      toValue: posV.current,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
+    if(posV.current < 6){
+      posV.current += 1
+      Animated.timing(moveV, {
+        toValue: posV.current,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }
   }
   function left() {
-    posH.current -= 1
-    Animated.timing(moveH, {
-      toValue: posH.current,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
+    if(posH.current > 0){
+      posH.current -= 1
+      Animated.timing(moveH, {
+        toValue: posH.current,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }
   }
   function right() {
-    posH.current += 1
-    Animated.timing(moveH, {
-      toValue: posH.current,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
+    if(posH.current < 6){
+      posH.current += 1
+      Animated.timing(moveH, {
+        toValue: posH.current,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }
   }
+
+  function tick() {
+    if(direction === 'up')
+      up()
+    if(direction === 'left')
+      left()
+    if(direction === 'right')
+      right()
+    if(direction === 'down')
+      down()
+
+  }
+
+  useInterval(tick, 500)
 
   return (
     <SafeAreaView style={styles.main}>
-      <Animated.View style={{
-        height: 50, aspectRatio: 1, backgroundColor: 'red', zIndex: 1,
-        transform: [
-          { translateX: moveH.interpolate({ inputRange: [0, 1], outputRange: [0, 50] }) },
-          { translateY: moveV.interpolate({ inputRange: [0, 1], outputRange: [0, 50] }) }
-        ]
-      }}
-      />
+      
       <View style={styles.board}>
+        <Animated.View style={{
+          height: 50, aspectRatio: 1, backgroundColor: 'red', zIndex: 1, position: 'absolute',
+          transform: [
+            { translateX: moveH.interpolate({ inputRange: [0, 1], outputRange: [0, 50] }) },
+            { translateY: moveV.interpolate({ inputRange: [0, 1], outputRange: [0, 50] }) }
+          ]
+        }}
+        />
         {tiles}
       </View>
       <View>
-        <Button title='up' onPress={up} />
+        <Button title='up' onPress={() => setDirection('up')} />
         <View style={{ flexDirection: 'row' }}>
           <View style={{ width: '50%' }}>
-            <Button title='left' onPress={left} />
+            <Button title='left' onPress={() => setDirection('left')} />
           </View>
           <View style={{ width: '50%' }}>
-            <Button title='right' onPress={right} />
+            <Button title='right' onPress={() => setDirection('right')} />
           </View>
         </View>
-        <Button title='down' onPress={down} />
+        <Button title='down' onPress={() => setDirection('down')} />
       </View>
     </SafeAreaView>
   );
