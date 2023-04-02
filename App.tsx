0ebe6406d6759e7eda5afdd44560daf8d43fd743
Salih Bektaş <sbektas@gtu.edu.tr>
2023-04-02 19:11:25 +0300

    fix: movement is prevented from reversing

--- App.tsx
+++ App.tsx
@@ -27,17 +27,17 @@ function App(): JSX.Element {
 
 
   function tick() {
-    if(direction === 'up' && posV.current > 0)
+    if (direction === 'up' && posV.current > 0)
       posV.current -= 50
-    if(direction === 'left' && posH.current > 0)
+    if (direction === 'left' && posH.current > 0)
       posH.current -= 50
-    if(direction === 'right' && posH.current < 300)
+    if (direction === 'right' && posH.current < 300)
       posH.current += 50
-    if(direction === 'down' && posV.current < 300)
+    if (direction === 'down' && posV.current < 300)
       posV.current += 50
 
     Animated.timing(head, {
-      toValue: {x: posH.current, y: posV.current},
+      toValue: { x: posH.current, y: posV.current },
       duration: 500,
       useNativeDriver: true,
     }).start();
@@ -47,7 +47,7 @@ function App(): JSX.Element {
 
   return (
     <SafeAreaView style={styles.main}>
-      
+
       <View style={styles.board}>
         <Animated.View style={{
           height: 50, aspectRatio: 1, backgroundColor: 'red', zIndex: 1, position: 'absolute',
@@ -60,16 +60,16 @@ function App(): JSX.Element {
         {tiles}
       </View>
       <View>
-        <Button title='up' onPress={() => setDirection('up')} />
+        <Button title='up' onPress={() => { if (direction !== 'down') setDirection('up') }} />
         <View style={{ flexDirection: 'row' }}>
           <View style={{ width: '50%' }}>
-            <Button title='left' onPress={() => setDirection('left')} />
+            <Button title='left' onPress={() => { if (direction !== 'right') setDirection('left') }} />
           </View>
           <View style={{ width: '50%' }}>
-            <Button title='right' onPress={() => setDirection('right')} />
+            <Button title='right' onPress={() => { if (direction !== 'left') setDirection('right') }} />
           </View>
         </View>
-        <Button title='down' onPress={() => setDirection('down')} />
+        <Button title='down' onPress={() => { if (direction !== 'up') setDirection('down') }} />
       </View>
     </SafeAreaView>
   );
