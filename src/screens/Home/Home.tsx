92b9387dfb222299d29611f5c4b3868b73259cd3
Salih Bektaş <sbektas@gtu.edu.tr>
2023-04-15 22:56:31 +0300

    feat: avatar and user name added

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
+  Image,
   SafeAreaView,
   StyleSheet,
-  TextInput
+  Text,
+  TextInput,
+  View
 } from 'react-native';
 
 import { HomeProps } from '../../types';
+import * as Cheerio from 'cheerio';
 
 
 
 
 function Home({ navigation }: HomeProps): JSX.Element {
 
-  function onChange(userName: string) {
-    console.log(userName)
+  const [user, setUser] = useState('')
+  const [avatar, setAvatar] = useState<string | undefined>('')
+
+
+  function getData(username: string) {
+    fetch(`https://github.com/${username}`).then(value => value.text()).then(value => {
+      const $ = Cheerio.load(value);
+      const $avatar = $('.avatar.avatar-user.width-full.border.color-bg-default');
+      const $name = $('.p-name.vcard-fullname.d-block.overflow-hidden');
+      setUser($name.text())
+      setAvatar($avatar.attr('src'))
+    })
   }
 
   return (
     <SafeAreaView style={styles.main}>
-      <TextInput onChangeText={onChange} placeholder='UserName' style={{backgroundColor: 'white'}} />
+
+      <View style={{flexDirection: 'row', paddingHorizontal: 16, justifyContent: 'space-around'}}>
+        <Text style={{color: 'white', fontSize: 24}}>{user}</Text>
+        <Image src={avatar} style={{width: 120, height: 120, borderRadius: 60}}/>
+      </View>
+      <TextInput onChangeText={getData} placeholder='UserName' style={{backgroundColor: 'white'}} />
 
       <Button title='Go Game' onPress={() => navigation.navigate('Game') }/>
       
