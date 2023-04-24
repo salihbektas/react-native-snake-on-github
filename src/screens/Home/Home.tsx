0a351b3330da01679afcdceed469edbc4a46bdd4
Salih Bektaş <sbektas@gtu.edu.tr>
2023-04-24 23:31:05 +0300

    feat: useTransition added

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState, useTransition } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -19,22 +19,30 @@ function Home({ navigation }: HomeProps): JSX.Element {
   const [nickName, setNickName] = useState('')
   const [avatar, setAvatar] = useState<string | undefined>('')
   const [data, setData] = useState('')
+  const [isPending, startTransition] = useTransition()
+
+  const abort = useRef(new AbortController())
 
 
   function getData(username: string) {
-    fetch(`https://github.com/${username}`).then(value => value.text()).then(value => {
-      const $ = Cheerio.load(value);
-      const $days = $("svg.js-calendar-graph-svg rect.ContributionCalendar-day");
-      const $avatar = $('.avatar.avatar-user.width-full.border.color-bg-default');
-      const $name = $('.p-name.vcard-fullname.d-block.overflow-hidden');
-      const $nickname = $('.p-nickname.vcard-username.d-block')
-      setUser($name.text().trim())
-      setNickName($nickname.text().trim())
-      setAvatar($avatar.attr('src'))
-      let newData : string = ''
-      $($days.get()).each((i, day) => newData += $(day).attr('data-level')+',')
-      newData += $name.text().trim() + ',' + $nickname.text().trim() + ',' + $avatar.attr('src')
-      setData(newData)
+    abort.current.abort()
+    abort.current = new AbortController()
+    let signal = abort.current.signal
+    startTransition(() => {
+      fetch(`https://github.com/${username}`, {signal}).then(value => value.text()).then(value => {
+        const $ = Cheerio.load(value);
+        const $days = $("svg.js-calendar-graph-svg rect.ContributionCalendar-day");
+        const $avatar = $('.avatar.avatar-user.width-full.border.color-bg-default');
+        const $name = $('.p-name.vcard-fullname.d-block.overflow-hidden');
+        const $nickname = $('.p-nickname.vcard-username.d-block')
+        setUser($name.text().trim())
+        setNickName($nickname.text().trim())
+        setAvatar($avatar.attr('src'))
+        let newData : string = ''
+        $($days.get()).each((i, day) => newData += $(day).attr('data-level')+',')
+        newData += $name.text().trim() + ',' + $nickname.text().trim() + ',' + $avatar.attr('src')
+        setData(newData)
+      }).catch(e => console.log(signal.aborted))
     })
   }
 
