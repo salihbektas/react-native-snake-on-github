9e0a7762ee36e161988116e2422632e1a21e2822
salihbektas <sbektas@gtu.edu.tr>
2025-03-09 16:10:40 +0300

    feat: Updated .env template and README for GitHub token

--- src/ApolloClient.tsx
+++ src/ApolloClient.tsx
@@ -1,6 +1,13 @@
 import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
 
+
+if (!process.env.GITHUB_TOKEN) {
+  throw new Error(
+    'Missing github token. Please set github token in your .env.template',
+  );
+}
+
 const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
 });
