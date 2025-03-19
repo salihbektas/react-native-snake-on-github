0eddcf65dfef3220b7e24943cabb729eb72a2c7a
salihbektas <sbektas@gtu.edu.tr>
2025-03-19 13:02:30 +0300

    Refactor: Replace SkeletonPlaceholder with ActivityIndicator in LoadingCard component

--- src/components/LoadingCard.tsx
+++ src/components/LoadingCard.tsx
@@ -1,43 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 
 function LoadingCard() {
   return (
     <View style={styles.main}>
       <Text style={styles.text}>Looking Profile</Text>
-      <SkeletonPlaceholder backgroundColor={'#0d1117'} speed={2000}>
-        <SkeletonPlaceholder.Item flexDirection="row" alignItems="center">
-          <SkeletonPlaceholder.Item
-            width={120}
-            height={20}
-            borderRadius={10}
-            marginRight={40}
-          />
-          <SkeletonPlaceholder.Item
-            width={120}
-            height={120}
-            borderRadius={60}
-          />
-        </SkeletonPlaceholder.Item>
-      </SkeletonPlaceholder>
+      <ActivityIndicator size='large' color='#26a641'/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   main: {
-    flexDirection: 'row',
-    marginLeft: 'auto',
-    marginVertical: 8,
+    alignItems: 'center',
+    height: 136,
+    justifyContent: 'space-evenly',
   },
 
   text: {
-    position: 'absolute',
-    top: 0,
-    right: 120,
     color: 'white',
-    fontSize: 24,
+    fontSize: 24
   },
 });
 
